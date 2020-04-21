5975da88ac50daecf553756b0bfdba05b6f5d072
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2020-04-20 22:26:37 -0600

    Changed module naming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="reorder-pdf",
+    name="ReorderPDF",
     version="0.0.1",
     packages=find_packages(),
     install_requires=["pypdf2"],
